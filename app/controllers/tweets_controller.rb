f568194058fd9d7790e9d30acfa0948c77b302c5
philip greenwald <philipgreenwald@philips-MacBook-Pro.local>
2014-10-18 16:54:43 -0400

    created validations for tweets and flash messages

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -6,7 +6,14 @@ class TweetsController < ApplicationController
 
 	def create
 		@tweet_to_create = Tweet.new(tweet_params)
-		@tweet_to_create.save
+		if @tweet_to_create.save
+			flash.now["success"] = "You have created a tweet"
+		else
+			flash.now["danger"] = "Please review the errors below."
+		end
+
+
+
 
 		@tweet = Tweet.new
 
